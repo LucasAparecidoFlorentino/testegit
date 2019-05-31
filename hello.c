2f5edcb18a1a9532e55a1234356a388b06c31dca
Lucas Florentino <lucasflorentino_2000@hotmail.com>
2019-05-31 20:22:59 -0300

    adicionei ponteiros e readme

--- hello.c
+++ hello.c
@@ -2,7 +2,20 @@
 
 int main ()
 {
-    printf("Olá Mundo !\n");
+
+    int idade, *p;
+
+    idade = 18; 
+
+    p = &idade; 
+
+    *p = 22;
+
+    idade = (int)p;  
+
+    printf("Olá Mundo !, a idade é %i\n", idade);
+    
+    printf("O endereço da variavel idade é; %p\n", p);
 
 
     return 0;
